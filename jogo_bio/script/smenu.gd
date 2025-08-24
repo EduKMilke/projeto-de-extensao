eade81b407b65fbb98fc51a387c1c13f435e2827
Eduardo K. Milke <edukmilke@gmail.com>
2025-08-24 14:19:21 -0300

    melhora das cartas

---
+++ jogo_bio/script/smenu.gd
@@ -0,0 +1,10 @@
+extends Control
+@onready var play=$play
+@onready var sair=$sair
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/escolha_s.tscn")
