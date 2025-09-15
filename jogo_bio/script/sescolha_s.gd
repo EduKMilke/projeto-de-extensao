3184a926435177330b23caf625e346407e062499
Eduardo K. Milke <edukmilke@gmail.com>
2025-09-15 19:08:16 -0300

    mudanca botao dificuldade

--- jogo_bio/script/sescolha_s.gd
+++ jogo_bio/script/sescolha_s.gd
@@ -2,16 +2,20 @@ extends Control
 
 @onready var b_dif=$dific
 @onready var label_dif=$dific/Label
+
 var n_dific=["Fácil","Médio","Difícil"]
+var c_dific=["00ba00","ff8200","de0000"]
 
 
 func _ready() -> void:
 	label_dif.text=n_dific[Global.dific] 
+	b_dif.modulate=c_dific[Global.dific] 
 func _on_dific_pressed() -> void: #mudar a dificuladade
 		Global.dific+=1
 		if Global.dific>2:
 			Global.dific=0
 		label_dif.text=n_dific[Global.dific]
+		b_dif.modulate=c_dific[Global.dific]
 #botões de para cada sistema
 func _on_b_s_diges_pressed() -> void:
 	Global.s_dige=true
