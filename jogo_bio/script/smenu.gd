e5f9e3f3ee155e4929a077b1d4ca33e7c6d82d9a
Eduardo K. Milke <edukmilke@gmail.com>
2025-09-19 17:43:53 -0300

    falta música

--- jogo_bio/script/smenu.gd
+++ jogo_bio/script/smenu.gd
@@ -1,10 +1,9 @@
 extends Control
 @onready var play=$play
 @onready var sair=$sair
-
-func _process(delta: float) -> void:
-	pass
-
+@onready var anime=$SBioblink/AnimationPlayer
+func _ready() -> void:
+	anime.play("new_animation")
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/escolha_s.tscn")
