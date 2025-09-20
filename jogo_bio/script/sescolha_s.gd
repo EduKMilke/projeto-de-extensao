5a0898031559d74f17789c2fbfef3ab58f8731c6
Eduardo K. Milke <edukmilke@gmail.com>
2025-09-20 15:18:59 -0300

    versao final

--- jogo_bio/script/sescolha_s.gd
+++ jogo_bio/script/sescolha_s.gd
@@ -52,3 +52,7 @@ func _on_b_s_esque_pressed() -> void:
 func _on_b_s_nerv_pressed() -> void:
 	Global.s_nerv=true
 	get_tree().change_scene_to_file("res://cenas/jogo.tscn")
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://cenas/menu.tscn")
