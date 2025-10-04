9052fc2926ad0aa50de32e0234b3cc86932d48da
Eduardo K. Milke <edukmilke@gmail.com>
2025-10-04 15:50:22 -0300

    atlz de sprites

--- jogo_bio/script/sescolha_s.gd
+++ jogo_bio/script/sescolha_s.gd
@@ -5,11 +5,12 @@ extends Control
 
 var n_dific=["Fácil","Médio","Difícil"]
 var c_dific=["00ba00","ff8200","de0000"]
-
+@onready var subt=$"Título(1)/AnimationPlayer"
 
 func _ready() -> void:
 	label_dif.text=n_dific[Global.dific] 
 	b_dif.modulate=c_dific[Global.dific] 
+	subt.play("new_animation")
 func _on_dific_pressed() -> void: #mudar a dificuladade
 		Global.dific+=1
 		if Global.dific>2:
